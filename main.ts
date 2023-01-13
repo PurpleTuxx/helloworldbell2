c1993414d3f23a1ec2ac8e1d9e933bec070665c7
PurpleTuxx <122543467+PurpleTuxx@users.noreply.github.com>
2023-01-13 09:56:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ input.onButtonPressed(Button.A, function () {
         # . . . #
         . # # # .
         `)
+    music.playMelody("D E F G A F E D ", 315)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
@@ -15,13 +16,35 @@ input.onButtonPressed(Button.B, function () {
         . # # # .
         # . . . #
         `)
+    music.playMelody("B B F F F C C C ", 244)
 })
 basic.showLeds(`
     . . . . .
-    . . . . #
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.showLeds(`
+    . # # # .
     . . . # .
-    # . # . .
-    . # . . .
+    . . # . .
+    . . . . .
+    . . # . .
     `)
 basic.forever(function () {
 	
