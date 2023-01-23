5d609932c0a943bc86ab63932b23d2c3ac98f8a4
PurpleTuxx <122543467+PurpleTuxx@users.noreply.github.com>
2023-01-23 09:41:57 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,27 @@ input.onButtonPressed(Button.A, function () {
         `)
     music.playMelody("D E F G A F E D ", 315)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . . # #
+        . # # # #
+        . # . # .
+        `)
+    music.playTone(277, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Double))
+    music.playTone(277, music.beat(BeatFraction.Quarter))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    music.playTone(156, music.beat(BeatFraction.Whole))
+})
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
         . . . . .
@@ -18,6 +39,47 @@ input.onButtonPressed(Button.B, function () {
         `)
     music.playMelody("B B B G G G C C ", 283)
 })
+input.onGesture(Gesture.Shake, function () {
+    RPS = randint(1, 3)
+    if (RPS == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (RPS == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    while (input.logoIsPressed()) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `,0)
+    }
+    basic.clearScreen()
+})
+let RPS = 0
 basic.showLeds(`
     . . . . .
     . . . . .
@@ -46,15 +108,4 @@ basic.showLeds(`
     . . . . .
     . . # . .
     `,1000)
-basic.forever(function () {
-    while (input.logoIsPressed()) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `,0)
-    }
-    basic.clearScreen()
-})
+basic.clearScreen()
